c1becda7c03f067417a0eea768bf2c245c4e64be
Sateeshnallamothu <sknallamothu@hotmail.com>
2016-12-15 16:42:25 -0600

    3ed commit

--- plot2.R
+++ plot2.R
@@ -10,9 +10,12 @@ cn<-c("Date" , "Time" , "Global_active_power","Global_reactive_power","Voltage" 
 
 hh.power.con<-fread("household_power_consumption.txt") %>% 
   filter(Date == '2/1/2007' | Date=='2/2/2007' ) 
-hh.power.con$Date <- as.Date(hh.power.con$Date,"%m/%d/%Y")
+##hh.power.con$DateTime<-paste(hh.power.con$Date,hh.power.con$Time,sep = ' ')
+hh.power.con$DateTime<-strptime(paste(hh.power.con$Date,hh.power.con$Time,sep = ' '),"%m/%d/%Y %H:%M:%S")
+##hh.power.con$Date <- as.Date(hh.power.con$Date,"%m/%d/%Y")
+
 ##hh.power.con$Time <- strptime(hh.power.con$Time,"%H:%M:%S")
-hist(table(hh.power.con$Global_active_power),col='red',xlim=c(1,10),ylim=c(0,600),
-     breaks=c(0,seq(2,50,2)),xlab='Global Active Power (kilowatts)',main='Global Active Power')
-dev.copy(png,file='exploratoryanalysis/plot1.png')
+plot(hh.power.con$DateTime,hh.power.con$Global_active_power,type="l",
+    ylab='Global Active Power (kilowatts)',main='Global Active Power',xlab = '')
+dev.copy(png,file='exploratoryanalysis/plot2.png')
 dev.off()
